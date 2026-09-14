e10366c7e7d8cb152af952dfaf07e942b54e7bde
Sai kiran <saikiran29.u@gmail.com>
2026-09-14 16:59:50 +0530

    convert RAG from gitlink to regular directory

---
+++ RAG/src/index.js
@@ -0,0 +1,27 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+
+const key = " ";
+// console.log(apiKey)
+async function pdf(filePath) {
+  const loader = new PDFLoader(filePath);
+  const document = await loader.load();
+
+  const embeddings = new OpenAIEmbeddings({
+    model: "text-embedding-3-small",
+    apiKey: key,
+  });
+
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: "http://localhost:6333",
+      collectionName: "bedofprocrustes",
+    },
+  );
+
+  vectorStore.addDocuments(document);
+}
+
+pdf("bedofprocrustes.pdf");
